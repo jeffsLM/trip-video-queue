b3fb7a6379b665500d0a87bd0711cf89c71074b4
jeffsLM <jeffes.dev@gmail.com>
2026-01-26 14:38:42 -0300

    chore: Update ecosystem configuration to use relative paths and add logging for output and errors

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,8 +2,7 @@ module.exports = {
   apps: [
     {
       name: "trip-video-queue",
-      script: "dist/index.js",
-      cwd: "c:/source/trip-video-queue",
+      script: "./dist/index.js",
       instances: 1,
       autorestart: true,
       watch: false,
@@ -12,6 +11,9 @@ module.exports = {
         NODE_ENV: "production",
       },
       log_date_format: "YYYY-MM-DD HH:mm:ss",
+      error_file: "logs/error.log",
+      out_file: "logs/out.log",
+      merge_logs: true,
     }
   ],
 };
